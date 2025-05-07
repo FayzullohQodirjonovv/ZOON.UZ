29fa1e8fc2db0c456011ea81edf09dc2c34ce3bb
fayzulloqodirjonoff <fqodirjonov1@gmail.com>
2025-05-07 16:18:29 +0500

    hello

--- src/assets/components/koko/index.jsx
+++ src/assets/components/koko/index.jsx
@@ -6,12 +6,12 @@ import forr from '../../../assets/4chi.svg'
 
 const Koko = () => {
   return (
-    <div className="flex justify-between w-[90%] m-auto mt-[40px] flex-wrap gap-4 mb-[30px]">
-      <img src={one} alt="logo1" className="max-w-[280px] w-full object-contain" />
-      <img src={two} alt="logo2" className="max-w-[280px] w-full object-contain" />
-      <img src={thre} alt="logo3" className="max-w-[280px] w-full object-contain" />
-      <img src={forr} alt="logo4" className="max-w-[280px] w-full object-contain" />
-    </div>
+    <div className="flex justify-center w-[90%] m-auto mt-[40px] flex-wrap gap-4 mb-[30px]">
+    <img src={one} alt="logo1" className="max-w-[280px] w-full object-contain" />
+    <img src={two} alt="logo2" className="max-w-[280px] w-full object-contain" />
+    <img src={thre} alt="logo3" className="max-w-[280px] w-full object-contain" />
+    <img src={forr} alt="logo4" className="max-w-[280px] w-full object-contain" />
+  </div>
   )
 }
 
